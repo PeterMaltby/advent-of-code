532e2f2408ac63b757b09d924561a13ceba81341
Peter Maltby <peter.maltby1@sainsburys.co.uk>
2023-11-19 19:45:38 +0000

    day 12

--- src/bin/2022_day12.rs
+++ src/bin/2022_day12.rs
@@ -1,3 +1,4 @@
+use std::collections::{self, VecDeque};
 use std::path::{Path, PathBuf};
 use std::time::Instant;
 mod utils;
@@ -26,6 +27,9 @@ struct Map {
     distances: Vec<Vec<u32>>,
     start: (usize, usize),
     end: (usize, usize),
+    max_x: usize,
+    max_y: usize,
+    first_a: Option<(usize, usize)>,
 }
 
 impl Map {
@@ -60,7 +64,15 @@ impl Map {
             }
         }
 
-        return Map { map, distances, start, end };
+        return Map {
+            max_x: map.len(),
+            max_y: map[0].len(),
+            map,
+            distances,
+            start,
+            end,
+            first_a: None,
+        };
     }
 
     fn print(&self) {
@@ -87,7 +99,7 @@ impl Map {
             }
             print!("|\n");
             x += 1;
-            y =0;
+            y = 0;
         }
         println!("start: {:?}, end: {:?}", self.start, self.end);
     }
@@ -95,34 +107,191 @@ impl Map {
     fn print_distance(&self) {
         for row in &self.distances {
             for char in row {
-                if *char > 99 {
-                    print!("|9+");
-                } else if *char > 9 {
+                if *char > 999 {
+                    print!("|99+");
+                } else if *char > 99 {
                     print!("|{}", char);
-                } else {
+                } else if *char > 9 {
                     print!("|0{}", char);
+                } else {
+                    print!("|00{}", char);
                 }
             }
             print!("|\n");
             for _ in 0..row.len() {
-                print!("+--");
+                print!("+---");
             }
             print!("|\n");
         }
+        print!("\n\n");
     }
 
+    fn print_both(&self) {
+        for x in 0..self.map.len() {
+            for char in &self.map[x] {
+                if *char > 999 {
+                    print!("|99+");
+                } else if *char > 99 {
+                    print!("|{}", char);
+                } else if *char > 9 {
+                    print!("|0{}", char);
+                } else {
+                    print!("|00{}", char);
+                }
+            }
+            print!("|\n");
+            for char in &self.distances[x] {
+                if *char > 999 {
+                    print!("|99+");
+                } else if *char > 99 {
+                    print!("|{}", char);
+                } else if *char > 9 {
+                    print!("|0{}", char);
+                } else {
+                    print!("|00{}", char);
+                }
+            }
+            print!("|\n");
+            for _ in 0..self.distances[0].len() {
+                print!("+---");
+            }
+            print!("|\n");
+        }
+        println!("\n");
+    }
+
+    fn is_valid(&self, x: usize, y: usize, height: u32) -> bool {
+        if x >= self.max_x || y >= self.max_y {
+            return false;
+        };
+        let this_height = self.map[x][y];
+        if self.distances[x][y] != std::u32::MAX {
+            return false;
+        };
+        if this_height <= height + 1 {
+            return true;
+        };
+        return false;
+    }
+
+    fn is_valid2(&mut self, x: usize, y: usize, height: u32) -> bool {
+        if x >= self.max_x || y >= self.max_y {
+            return false;
+        };
+        let this_height = self.map[x][y];
+
+        if self.distances[x][y] != std::u32::MAX {
+            return false;
+        };
+        if this_height >= height - 1 {
+            if this_height == 1 && self.first_a == None {
+                self.first_a = Some((x, y));
+            }
+
+            return true;
+        };
+        return false;
+    }
+
+    fn solve_distances(&mut self) {
+        let mut to_solve: std::collections::VecDeque<(usize, usize)> = VecDeque::new();
+
+        self.distances[self.start.0][self.start.1] = 0;
+        to_solve.push_back(self.start);
+
+        loop {
+            let coords = to_solve.pop_front();
+            match coords {
+                None => break,
+                Some((x, y)) => {
+                    let height = self.map[x][y];
+                    let distance = self.distances[x][y];
+
+                    if x != 0 {
+                        if self.is_valid(x - 1, y, height) {
+                            self.distances[x - 1][y] = distance + 1;
+                            to_solve.push_back((x - 1, y));
+                        }
+                    }
+                    if self.is_valid(x + 1, y, height) {
+                        self.distances[x + 1][y] = distance + 1;
+                        to_solve.push_back((x + 1, y));
+                    }
+                    if y != 0 {
+                        if self.is_valid(x, y - 1, height) {
+                            self.distances[x][y - 1] = distance + 1;
+                            to_solve.push_back((x, y - 1));
+                        }
+                    }
+                    if self.is_valid(x, y + 1, height) {
+                        self.distances[x][y + 1] = distance + 1;
+                        to_solve.push_back((x, y + 1));
+                    }
+                }
+            }
+        }
+    }
+
+    fn solve_distances2(&mut self) {
+        let mut to_solve: std::collections::VecDeque<(usize, usize)> = VecDeque::new();
+
+        self.distances[self.end.0][self.end.1] = 0;
+        to_solve.push_back(self.end);
+
+        loop {
+            let coords = to_solve.pop_front();
+            match coords {
+                None => break,
+                Some((x, y)) => {
+                    let height = self.map[x][y];
+                    let distance = self.distances[x][y];
+
+                    if x != 0 {
+                        if self.is_valid2(x - 1, y, height) {
+                            self.distances[x - 1][y] = distance + 1;
+                            to_solve.push_back((x - 1, y));
+                        }
+                    }
+                    if self.is_valid2(x + 1, y, height) {
+                        self.distances[x + 1][y] = distance + 1;
+                        to_solve.push_back((x + 1, y));
+                    }
+                    if y != 0 {
+                        if self.is_valid2(x, y - 1, height) {
+                            self.distances[x][y - 1] = distance + 1;
+                            to_solve.push_back((x, y - 1));
+                        }
+                    }
+                    if self.is_valid2(x, y + 1, height) {
+                        self.distances[x][y + 1] = distance + 1;
+                        to_solve.push_back((x, y + 1));
+                    }
+                }
+            }
+        }
+    }
+
+    fn get_sol(&self) -> u32 {
+        return self.distances[self.end.0][self.end.1];
+    }
+
+    fn get_sol2(&self) -> u32 {
+        match self.first_a {
+            None => panic!("no first a!"),
+            Some(a) => return self.distances[a.0][a.1],
+        }
+    }
 }
 
-fn task1(file: &Path) -> i32 {
-    let mut n = 0;
-
-    let map: Map = Map::new(file);
-    map.print();
-    map.print_distance();
-
-    return n;
+fn task1(file: &Path) -> u32 {
+    let mut map: Map = Map::new(file);
+    map.solve_distances();
+    return map.get_sol();
 }
 
-fn task2(file: &Path) -> i32 {
-    return 32;
+fn task2(file: &Path) -> u32 {
+    let mut map: Map = Map::new(file);
+    map.solve_distances2();
+
+    return map.get_sol2();
 }
